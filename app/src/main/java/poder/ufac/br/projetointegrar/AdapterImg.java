0d3b68e4c1e7bae514a2d28d097e27e6cab4c00f
Levi Cacau <levicacau@gmail.com>
2015-09-29 21:49:39 -0500

    Criação do ViewPage, paginas com scroll

---
+++ app/src/main/java/poder/ufac/br/projetointegrar/AdapterImg.java
@@ -0,0 +1,67 @@
+package poder.ufac.br.projetointegrar;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class AdapterImg extends PagerAdapter {
+    private Context context;
+    private int[] imgs;
+    private int[] audio;
+
+    public AdapterImg(Context context, int[] imgs){
+        this.context = context;
+        this.imgs = imgs;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return imgs.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object obj) {
+        // TODO Auto-generated method stub
+//        Log.i("Script", "view == obj: "+((view == obj) ? "1" : "0"));
+//        Log.i("Script", "view == ((TextView) obj).getParent(): "+((view == ((TextView) obj).getParent()) ? "1" : "0"));
+
+        return view == obj;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position){
+
+        LinearLayout ll = new LinearLayout(context);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        ll.setLayoutParams(lp);
+        container.addView(ll);
+
+        ImageView iv = new ImageView(context);
+        iv.setImageResource(imgs[position]);
+        ll.addView(iv);
+
+        TextView tv = new TextView(context);
+        tv.setText("Carro " + (position + 1));
+        ll.addView(tv);
+//        Log.i("Script", "Build: Carro: "+(position + 1));
+
+        return(ll);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object view){
+//        Log.i("Script", "Destroy: Carro: "+(position + 1));
+//        container.removeView((View)((TextView)view).getParent());
+        container.removeView((View)view);
+    }
+
+}
--- app/src/main/java/poder/ufac/br/projetointegrar/MainActivity.java
+++ app/src/main/java/poder/ufac/br/projetointegrar/MainActivity.java
@@ -1,37 +1,74 @@
 package poder.ufac.br.projetointegrar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    MediaPlayer player;
+    private int[] carros = {R.drawable.ferrari_laferrari, R.drawable.bugatti_veyron, R.drawable.pagani_zonda,
+            R.drawable.porsche_911, R.drawable.mclaren, R.drawable.maserati_gran_turismo,
+            R.drawable.lamborghini_veneno};
+
+    private int [] escovando_dentes = {
+            R.drawable.escovando_dentes_01, R.drawable.escovando_dentes_02, R.drawable.escovando_dentes_03,
+            R.drawable.escovando_dentes_04, R.drawable.escovando_dentes_05, R.drawable.escovando_dentes_06,
+            R.drawable.escovando_dentes_07, R.drawable.escovando_dentes_08
+    };
+
+    private int[] escovando_dentes_audio = {
+            R.raw.escovar_dentes_01, R.raw.escovar_dentes_02, R.raw.escovar_dentes_03, R.raw.escovar_dentes_04,
+            R.raw.escovar_dentes_05, R.raw.escovar_dentes_06, R.raw.escovar_dentes_07, R.raw.escovar_dentes_08
+    };
+
+    public void play(View v){
+        player= MediaPlayer.create(MainActivity.this, R.raw.escovar_dentes_02);
+        player.start();
+    }
+
+    public void pause(View v){
+        player.pause();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager vp = (ViewPager) findViewById(R.id.viewPager);
+//        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+//        vp.setLayoutParams(lp);
+
+        vp.setAdapter(new AdapterImg(this, escovando_dentes));
+
+//        vp.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//
+//            @Override
+//            public void onPageSelected(int arg0) {
+//                Log.i("Script", "onPageSelected()");
+//            }
+//
+//            @Override
+//            public void onPageScrolled(int arg0, float arg1, int arg2) {
+//                Log.i("Script", "onPageScrolled()");
+//
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int arg0) {
+//                Log.i("Script", "onPageScrollStateChanged()");
+//            }
+//        });
+
+//        LinearLayout ll = (LinearLayout) findViewById(R.id.LinearLayout1);
+//        ll.addView(vp);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
